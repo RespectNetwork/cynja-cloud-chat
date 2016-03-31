d317bb2026cda8d12213e7da69a9db56591c54ec
unknown <noopur.pandey@impetus-D835.impetus.co.in>
2016-04-01 02:25:16 +0530

    Signed-off-by: Noopur Pandey <noopur.pandey@impetus-D835.impetus.co.in> If a blocked connection is deleted, it is moved out of blocked list.

--- src/main/java/net/rn/clouds/chat/service/impl/ConnectionServiceImpl.java
+++ src/main/java/net/rn/clouds/chat/service/impl/ConnectionServiceImpl.java
@@ -1084,6 +1084,13 @@ public class ConnectionServiceImpl implements ConnectionService{
 
 							LOGGER.debug("Deleting connection request by reuquester");
 							connectionRequest.setDeleteRenew(DeleteRenew.DELETED_BY_REQUESTER.getDeleteRenew());
+							
+							if(status.equals(Status.BLOCKED_BY_REQUESTER.getStatus())){
+								connectionRequest.setStatus(Status.APPROVED.getStatus());
+							}else if(status.equals(Status.BLOCKED.getStatus())){
+								connectionRequest.setStatus(Status.BLOCKED_BY_ACCEPTOR.getStatus());
+							}
+							connectionRequest.setBlockedByRequester(null);
 							connectionRequestDAO.updateRequest(connectionRequest);
 
 						}else if (deleteRenew != null && (deleteRenew.equals(DeleteRenew.RENEWED_BY_ACCEPTOR.getDeleteRenew()))){
@@ -1120,6 +1127,13 @@ public class ConnectionServiceImpl implements ConnectionService{
 
 							LOGGER.debug("Deleting connection request by acceptor");
 							connectionRequest.setDeleteRenew(DeleteRenew.DELETED_BY_ACCEPTOR.getDeleteRenew());
+							
+							if(status.equals(Status.BLOCKED_BY_ACCEPTOR.getStatus())){
+								connectionRequest.setStatus(Status.APPROVED.getStatus());
+							}else if(status.equals(Status.BLOCKED.getStatus())){
+								connectionRequest.setStatus(Status.BLOCKED_BY_REQUESTER.getStatus());
+							}
+							connectionRequest.setBlockedByAcceptor(null);
 							connectionRequestDAO.updateRequest(connectionRequest);	
 
 						}else if (deleteRenew != null && deleteRenew.equals(DeleteRenew.RENEWED_BY_REQUESTER.getDeleteRenew())){
